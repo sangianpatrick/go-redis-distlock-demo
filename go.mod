54b637f2fb8b12b2abe5005fdfd56f02c374c2af
Patrick Maurits Sangian <patricksangian@gmail.com>
2020-09-26 06:55:12 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/sangianpatrick/go-redis-distlock-demo
+
+go 1.15
+
+require (
+	github.com/go-redis/redis/v8 v8.2.2
+	github.com/go-redsync/redsync v1.4.2
+	github.com/go-redsync/redsync/v4 v4.0.3
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/stvp/tempredis v0.0.0-20181119212430-b82af8480203
+)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v8"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	acquiredLockMessage    = "Acquired the lock."
+	notAcquiredLockMessage = "Resource is locked. Acquired by another request."
+)
+
+var rs *redsync.Redsync
+
+type result struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
+	pool := goredis.NewPool(redisClient)
+
+	rs = redsync.New(pool)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/healthcheck", handlerHealthCheck).Methods(http.MethodGet)
+	router.HandleFunc("/low", handlerWithLowLatency).Methods(http.MethodGet)
+	router.HandleFunc("/high", handlerWithHighLatency).Methods(http.MethodGet)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", port), router)
+}
+
+func handlerHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "Service is running properly.")
+}
+
+func handlerWithLowLatency(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	mx := rs.NewMutex("lock:global",
+		redsync.WithExpiry(time.Second*30),
+		redsync.WithTries(10),
+		redsync.WithRetryDelay(time.Second*1),
+	)
+
+	if err := mx.LockContext(ctx); err != nil {
+		res := result{
+			Success: false,
+			Message: notAcquiredLockMessage,
+		}
+		w.WriteHeader(http.StatusLocked)
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+	defer mx.UnlockContext(ctx)
+
+	time.Sleep(time.Millisecond * 500)
+	res := result{
+		Success: true,
+		Message: acquiredLockMessage,
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+	return
+}
+
+func handlerWithHighLatency(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	mx := rs.NewMutex("lock:global",
+		redsync.WithExpiry(time.Second*30),
+		redsync.WithTries(5),
+		redsync.WithRetryDelay(time.Second*1),
+	)
+
+	if err := mx.LockContext(ctx); err != nil {
+		res := result{
+			Success: false,
+			Message: notAcquiredLockMessage,
+		}
+		w.WriteHeader(http.StatusLocked)
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+	defer mx.UnlockContext(ctx)
+
+	time.Sleep(time.Second * 20)
+	res := result{
+		Success: true,
+		Message: acquiredLockMessage,
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+	return
+}
